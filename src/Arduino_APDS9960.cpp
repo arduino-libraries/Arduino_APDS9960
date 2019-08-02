cd55cfabe4ff08bc4b722c7d4355f5474785ead1
Sandeep Mistry <s.mistry@arduino.cc>
2019-08-02 11:15:18 -0400

    gestureAvailable + readGesture, add irq to constructor

--- src/Arduino_APDS9960.cpp
+++ src/Arduino_APDS9960.cpp
@@ -10,6 +10,13 @@ bool APDS9960::begin() {
     
   // Disable everything
   if (!setENABLE(0x00)) return false;
+  if (!setWTIME(0xFF)) return false;
+  if (!setGPULSE(0x8F)) return false; // 16us, 16 pulses // default is: 0x40 = 8us, 1 pulse
+  if (!setPPULSE(0x8F)) return false; // 16us, 16 pulses // default is: 0x40 = 8us, 1 pulse
+  if (!setGestureIntEnable(true)) return false;
+  if (!setGestureMode(true)) return false;
+  if (!enablePower()) return false;
+  if (!enableWait()) return false;
   // set ADC integration time to 10 ms
   if (!setATIME(256 - (10 / 2.78))) return false;
   // set ADC gain 4x (0x00 => 1x, 0x01 => 4x, 0x02 => 16x, 0x03 => 64x)
@@ -18,21 +25,21 @@ bool APDS9960::begin() {
   // enable power
   if (!enablePower()) return false;
 
+  pinMode(irqPin, INPUT);
+
   return true;
 }
 
-bool APDS9960::startGestureSensor() {
-  if (!setWTIME(0xFF)) return false;
-  if (!setGPULSE(0x8F)) return false; // 16us, 16 pulses // default is: 0x40 = 8us, 1 pulse
-  if (!setPPULSE(0x8F)) return false; // 16us, 16 pulses // default is: 0x40 = 8us, 1 pulse
-  if (!setGestureIntEnable(true)) return false;
-  if (!setGestureMode(true)) return false;
-  if (!enablePower()) return false;
-  if (!enableWait()) return false;
-  if (!enableProximity()) return false;
-  if (!enableGesture()) return false;
-  return true;
+void APDS9960::end() {
+  // Disable everything
+  setENABLE(0x00);
 }
+
+// bool APDS9960::startGestureSensor() {
+
+//   if (!enableGesture()) return false;
+//   return true;
+// }
 
 // Sets the LED current boost value:
 // 0=100%, 1=150%, 2=200%, 3=300%
@@ -171,7 +178,7 @@ size_t APDS9960::readBlock(uint8_t reg, uint8_t *val, unsigned int len) {
     return i;
 }
 
-int APDS9960::gestureAvailable() {
+int APDS9960::gestureFIFOAvailable() {
   uint8_t r;
   if (!getGSTATUS(&r)) return -1;
   if ((r & 0x01) == 0x00) return -2;
@@ -183,11 +190,12 @@ bool in = false;
 bool out = false;
 int direction = 0;
 int dir_in = 0;
+int gesture = -1;
 
 uint8_t threshold = 30;
 int APDS9960::handleGesture() {
   while (true) {
-    int available = gestureAvailable();
+    int available = gestureFIFOAvailable();
     if (available <= 0) return 0;
 
     uint8_t fifo_data[128];
@@ -199,7 +207,7 @@ int APDS9960::handleGesture() {
       uint8_t u,d,l,r;
       u = fifo_data[i++];
       d = fifo_data[i++];
-      l = fifo_data[i++];
+      l   = fifo_data[i++];
       r = fifo_data[i++];
       if (u>l && u>r && u>d) {
         direction = 1;
@@ -217,13 +225,13 @@ int APDS9960::handleGesture() {
       if (u<threshold && d<threshold && l<threshold && r<threshold) {
         in = true;
         if (direction != 0) {
-          Serial.print(" OUT ");
-          Serial.print(direction);
-          if (direction == 1 && dir_in == 2) Serial.print(" DOWN!");
-          if (direction == 2 && dir_in == 1) Serial.print(" UP!");
-          if (direction == 3 && dir_in == 4) Serial.print(" RIGHT!");
-          if (direction == 4 && dir_in == 3) Serial.print(" LEFT!");
-          Serial.println();
+          /*Serial.print(" OUT ");
+          Serial.print(direction);*/
+          if (direction == 1 && dir_in == 2) { /*Serial.print(" DOWN!");*/ gesture = 1; }
+          if (direction == 2 && dir_in == 1) { /*Serial.print(" UP!");*/ gesture = 2;}
+          if (direction == 3 && dir_in == 4) { /*Serial.print(" RIGHT!");*/ gesture = 3;}
+          if (direction == 4 && dir_in == 3) { /*Serial.print(" LEFT!");*/ gesture = 4;}
+          /*Serial.println();*/
           direction = 0;
           dir_in = 0;
         }
@@ -233,9 +241,9 @@ int APDS9960::handleGesture() {
       if (in && direction != 0) {
         in = false;
         dir_in = direction;
-        Serial.print("IN ");
+        /*Serial.print("IN ");
         Serial.print(direction);
-        Serial.print(" ");
+        Serial.print(" ");*/
       }
       // Serial.print(u);
       // Serial.print(",");
@@ -246,6 +254,26 @@ int APDS9960::handleGesture() {
       // Serial.println(r);
     }
   }
+}
+
+int APDS9960::gestureAvailable() {
+  enableGesture();
+
+  if (digitalRead(irqPin) != LOW) {
+    return 0;
+  }
+
+  handleGesture();
+
+  return (gesture > 0);
+}
+
+int APDS9960::readGesture() {
+  int result = gesture;
+
+  gesture = 0;
+
+  return result;
 }
 
 int APDS9960::colorAvailable() {
